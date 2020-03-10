bbb670b10f0693492e7c5fb32d45a6bb7d0e8fbe
Claire Filipek <cfilipek@sva.edu>
2020-03-10 06:01:22 -0400

    Updates

---
+++ precache-manifest.4b9736e89c3ac6c6e1e1827ba2fe80bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12a081d21758acada7e9f325008cc8d4",
+    "url": "/scrolling-blog/index.html"
+  },
+  {
+    "revision": "a9a82171a58e6fc699a0",
+    "url": "/scrolling-blog/static/css/main.34412088.chunk.css"
+  },
+  {
+    "revision": "fee1402025dee6155587",
+    "url": "/scrolling-blog/static/js/2.7a5b8426.chunk.js"
+  },
+  {
+    "revision": "b975e5a0089113c5dad8b1facbb557ee",
+    "url": "/scrolling-blog/static/js/2.7a5b8426.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9a82171a58e6fc699a0",
+    "url": "/scrolling-blog/static/js/main.21bc015d.chunk.js"
+  },
+  {
+    "revision": "831e60f648328e013e9a",
+    "url": "/scrolling-blog/static/js/runtime-main.e937982b.js"
+  },
+  {
+    "revision": "6bec892773c4314e17e167c00d155ec8",
+    "url": "/scrolling-blog/static/media/eye.6bec8927.png"
+  }
+]);
